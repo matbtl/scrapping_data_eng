5fa6e64085966d0db380f29853b2b92dcdc69536
matbtl <btlmmat020@gmail.com>
2022-01-27 05:30:40 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import redirect, url_for, Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def todo():
+    return "Hello!"
+
+@app.route('/new', methods=['POST'])
+def new():
+    return redirect(url_for('todo'))
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
